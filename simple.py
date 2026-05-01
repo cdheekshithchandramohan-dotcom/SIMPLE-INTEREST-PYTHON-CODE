363404edb1f1c7379ba11f02514c77e8635e4de5
DHEEKSHITH. C <cdheekshithchandramohan@gmail.com>
2026-05-01 23:12:34 +0530

    Fix print statement formatting in simple.py
    This simple python code shows your simple interest according to your details.

---
+++ simple.py
@@ -0,0 +1,10 @@
+# SIMPLE INTEREST PYTHON CODE
+
+print("simple interest calculation/n ")
+print("Enter the following details")
+p=int(input("amount:"))
+r=int(input("rate:"))
+t=int(input("time:"))
+SI=(p*r*t)/100
+print(SI)
+print("\n The interest is:")
